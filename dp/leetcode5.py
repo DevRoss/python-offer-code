3765d9018363f353980ec497a24fae8458b2a93e
DevRoss <820584458@qq.com>
2019-03-18 19:12:10 +0800

    最长回文串注释

--- dp/leetcode5.py
+++ dp/leetcode5.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 # Created by Ross on 19-3-18
 
+"""
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+示例 1：
+
+    输入: "babad"
+    输出: "bab"
+    注意: "aba" 也是一个有效答案。
+
+"""
+
+
 def solve(s: str):
     maxlen = 0
     start = 0
